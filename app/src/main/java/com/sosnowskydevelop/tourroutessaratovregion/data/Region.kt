7cd88569cbf29d2776e979cea387f1a28a88d57d
sosnovskiim <darkisosn2805@gmail.com>
2021-03-27 11:45:07 +0400

    Added id to region class.

--- app/src/main/java/com/sosnowskydevelop/tourroutessaratovregion/data/Region.kt
+++ app/src/main/java/com/sosnowskydevelop/tourroutessaratovregion/data/Region.kt
@@ -1,7 +1,9 @@
 package com.sosnowskydevelop.tourroutessaratovregion.data
 
 data class Region(
+    private val _id: Long,
     private val _name: String,
 ) {
+    val id: Long get() = _id
     val name: String get() = _name
 }
