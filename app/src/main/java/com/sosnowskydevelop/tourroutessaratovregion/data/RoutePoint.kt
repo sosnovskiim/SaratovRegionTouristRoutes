7cfbb71e11f49bf4beed2ccb38f8d9c53a7562e0
sosnovskiim <darkisosn2805@gmail.com>
2021-04-22 07:48:07 +0400

    Route map removed from route detail screen and added to route map screen and code style refactoring (some missing files added).

---
+++ app/src/main/java/com/sosnowskydevelop/tourroutessaratovregion/data/RoutePoint.kt
@@ -0,0 +1,15 @@
+package com.sosnowskydevelop.tourroutessaratovregion.data
+
+data class RoutePoint(
+    private val _id: Long,
+    private val _routeId: Long,
+    private val _latitude: Double,
+    private val _longitude: Double,
+    private val _name: String? = null,
+) {
+    val id: Long get() = _id
+    val routeId: Long get() = _routeId
+    val latitude: Double get() = _latitude
+    val longitude: Double get() = _longitude
+    val name: String? get() = _name
+}
